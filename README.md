1089a4363d9782ff60611c7fc8768bd9ba1ccf43
Edward George <edwardgeorge@gmail.com>
2024-06-02 21:06:26 +0100

    create bandcamp-wishlist
    forked from easlice/bandcamp-downloader at dd0d792dee185ebfa42c50235553eaed6a9e9665 'Update FUNDING.yml'.

---
+++ README.md
@@ -0,0 +1,90 @@
+# bandcamp-wishlist [![Python package](https://github.com/edwardgeorge/bandcamp-wishlist/actions/workflows/python-package.yml/badge.svg)](https://github.com/edwardgeorge/bandcamp-wishlist/actions/workflows/python-package.yml)
+
+Open random items from your Bandcamp wishlist in a browser. Based on and modified from the code from [https://github.com/easlice/bandcamp-downloader](https://github.com/easlice/bandcamp-downloader).
+
+Download your Bandcamp wishlist to a local JSON file and then query it for random recommendations which it will open in your browser to listen to.
+
+_The motivation for this script came from having a lengthy wishlist spanning many years yet when looking for new music to buy the bandcamp UI biases me into only purchasing music wishlisted in the recent past with older wishlisted items being essentially forgotten. I created this script to surface older items by opening a random item from my wishlist whenever run._
+
+It requires you to have a browser with a logged in session of bandcamp open. Cookies from the browser will be used to authenticate with Bandcamp.
+
+Supported browsers are the same as in [browser_cookie3](https://github.com/borisbabic/browser_cookie3): Chrome, Chromium, Firefox, Brave, Opera, and Edge
+
+Alternatively, you can use a [Netscape format cookies](https://curl.se/docs/http-cookies.html) file.
+
+## Usage
+
+First download your wishlist to a file, eg:
+```
+bandcamp-wishlist.py download -b firefox -o wishlist.json <bandcamp-username>
+```
+
+Then run to open a random album in your browser:
+```
+bandcamp-wishlist.py random -i wishlist.json
+```
+
+## Known Issues
+
+### Running the script on WSL crashes with a `DBUS_SESSION_BUS_ADDRESS` error
+
+This is seems to be a WSL issue. The browser_cookie3 module  tries to get a secret from your keyring via dbus, but WSL may not have dbus installed, or may not have it set up as expected. As such, you may see the following error:
+
+`secretstorage.exceptions.SecretServiceNotAvailableException: Environment variable DBUS_SESSION_BUS_ADDRESS is unset`
+
+Please either check your WSL dbus installation/configuration, or run the script nativity on windows.
+
+### "Unable to get key for cookie decryption" error, especially in Chrome
+
+There is currently an issue with [browser_cookie3](https://github.com/borisbabic/browser_cookie3). This has been reported within this repo [here](https://github.com/easlice/bandcamp-downloader/issues/17) and you can see the status of it upstream [here](https://github.com/borisbabic/browser_cookie3/issues/141).
+
+### "Failed to find <browser> cookie" even though you have the browser installed and are logged in.
+
+Sometimes a browser does not put its files in the expected location. This is especially true if the browser is installed as a flatpack or snap. As such, browser_cookie3 doesn't know where to look for the cookie store.
+
+You can fix this by using the `--cookies` flag and giving it the path to your browser's cookie store, usually a file named something like `Cookies` or `cookies.sqlite`. Note: You still need to give the correct `--browser` flag.
+
+Another option is to symlink the directory to the correct place. For example, the package `chromium-bin` often installs to the directory `~/.chromium-bin` but it is expected to be at `~.chromium`. You can run:
+`symlink -s ~/.chromium-bin ~/.chromium`
+and then browser_cookie3 will be able to find the cookies as expected and you will not need to use the `--cookies` flag.
+
+## Manual Setup
+
+Install the script dependencies by running:
+
+```
+pip install .
+```
+
+Run the program:
+
+```
+bandcamp-wishlist.py [arguments]
+```
+
+If you run into errors or dependency issues, you can try installing exact dependency versions by running:
+
+```
+pip install -r requirements.txt
+```
+
+## Setup via Poetry
+
+Install requirements using [Python Poetry](https://python-poetry.org/). [Installation instructions here](https://python-poetry.org/docs/#installation).
+
+```
+poetry install
+```
+
+Run the script within the poetry shell:
+
+```
+poetry shell
+python bandcamp-wishlist.py [arguments]
+```
+
+or directly through `poetry run`:
+
+```
+poetry run python bandcamp-wishlist.py [arguments]
+```
---
+++ bandcamp-wishlist.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+import html
+import http
+import http.cookiejar
+import json
+import logging
+import random
+import time
+import webbrowser
+from argparse import ArgumentParser
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List, Optional, Tuple
+
+import browser_cookie3
+import requests
+from bs4 import BeautifulSoup, SoupStrainer
+
+log = logging.getLogger()
+
+USER_URL = 'https://bandcamp.com/{}'
+WISHLIST_POST_URL = 'https://bandcamp.com/api/fancollection/1/wishlist_items'
+SUPPORTED_BROWSERS = [
+    'firefox',
+    'chrome',
+    'chromium',
+    'brave',
+    'opera',
+    'edge'
+]
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('--verbose', '-v', action='count', default = 0)
+    subp = parser.add_subparsers(dest="action", required=True)
+    dl = subp.add_parser('download')
+    dl.add_argument(
+        '--browser', '-b',
+        type=str,
+        default = 'firefox',
+        choices = SUPPORTED_BROWSERS,
+        help='The browser whose cookies to use for accessing bandcamp. Defaults to "firefox"'
+    )
+    dl.add_argument(
+        '--cookies', '-c',
+        type=Path,
+        help='Path to a cookie file. First, we will try to use it as a mozilla cookie jar. If that fails, it\'ll be used as the path for your given browser\'s cookie store.',
+    )
+    dl.add_argument(
+        '--output', '-o',
+        type=Path,
+    )
+    dl.add_argument("username")
+
+    ract = subp.add_parser("random")
+    ract.add_argument(
+        '--input', '-i',
+        type=Path,
+        required=True
+    )
+
+
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.DEBUG if args.verbose > 0 else logging.INFO)
+
+    if args.action == "download":
+        acc, d = get_user(args.username, args.browser, args.cookies)
+        ic = d['wishlist_data']['item_count']
+        print(f"getting {ic} wishlist items...")
+        wl = acc.get_wishlist(ic)
+        if args.output is not None:
+            with args.output.open('w') as f:
+                json.dump(wl, f)
+        else:
+            print(json.dumps(wl, indent=4))
+    elif args.action == "random":
+        with args.input.open() as f:
+            wl = json.load(f)
+        print("loaded {} items from {}".format(len(wl), args.input))
+
+        chosen = random.choice(wl)
+        print("Opening: {} '{}': {}\n(added on {})".format(
+            chosen['band_name'],
+            chosen['item_title'],
+            chosen['item_url'],
+            chosen["added"],
+        ))
+        webbrowser.open_new_tab(chosen['item_url'])
+
+
+@dataclass
+class Account:
+    fan_id: int
+    cookies: http.cookiejar.CookieJar
+
+    def _api_post(self, url: str, count: int, last_token: str):
+        payload = {
+            'fan_id' : self.fan_id,
+            'count' : count,
+            'older_than_token' : last_token,
+        }
+        with requests.post(
+            url,
+            data = json.dumps(payload),
+            cookies = self.cookies,
+        ) as response:
+            response.raise_for_status()
+            return json.loads(response.text)
+    
+    def _req_wishlist(self, count: int, last_token: str):
+        return self._api_post(WISHLIST_POST_URL, count=count, last_token=last_token)
+
+    def get_wishlist(self, count: int = 20) -> List[dict]:
+        r = []
+        tok = default_token()
+        while True:
+            d = self._req_wishlist(count=count, last_token=tok)
+            if 'error' in d:
+                raise Exception("Error from api request")
+            items = d['items']
+            r.extend(items)
+            tok = d['last_token']
+            log.debug("got batch of %s items. total: %s. last token: '%s'. more available? %s", len(items), len(r), tok, d['more_available'])
+            if not d['more_available']:
+                break
+        return r
+
+
+def default_token() -> str:
+    return '{}::a::'.format(int(time.time()))
+
+
+def get_user(username: str, browser: str, cookies: Optional[str]) -> Tuple[Account, dict]:
+    cjar = get_cookies(browser, cookies)
+    data = get_user_data(username, cjar)
+    acc = Account(data['fan_data']['fan_id'], cjar)
+    return (acc, data)
+
+
+def get_user_data(user: str, cookies: http.cookiejar.CookieJar) -> Optional[dict]:
+    soup = BeautifulSoup(
+        requests.get(
+            USER_URL.format(user),
+            cookies = cookies,
+        ).text,
+        'html.parser',
+        parse_only = SoupStrainer('div', id='pagedata'),
+    )
+    div = soup.find('div')
+    if not div:
+        raise Exception('ERROR: No div with pagedata found for user at url [{}]'.format(USER_URL.format(user)))
+    data = json.loads(html.unescape(div.get('data-blob')))
+    return data
+
+
+def get_cookies(browser: str, cookies: Optional[str] = None):
+    if cookies is not None:
+        return load_cookies_from_file(browser, cookies)
+    return load_cookies_from_browser(browser)
+
+
+def load_cookies_from_browser(browser: str):
+    try:
+        func = getattr(browser_cookie3, browser)
+    except AttributeError:
+        raise Exception('Browser type [{}] is unknown. Can\'t pull cookies, so can\'t authenticate with bandcamp.'.format(browser))
+    else:
+        return func(domain_name = 'bandcamp.com')
+
+
+def load_cookies_from_file(browser: str, cookies: str):
+    # First try it as a mozilla cookie jar
+    try:
+        cj = http.cookiejar.MozillaCookieJar(cookies)
+        cj.load()
+        return cj
+    except Exception as e:
+        log.info("Cookie file at [%s] not a mozilla cookie jar.\nTrying it as a cookie store for the browser [%s]...", cookies, browser)
+    # Next try it with browser_cookie
+    try:
+        func = getattr(browser_cookie3, browser)
+    except AttributeError:
+        raise Exception('Browser type [{}] is unknown. Can\'t pull cookies, so can\'t authenticate with bandcamp.'.format(browser))
+    else:
+        return func(domain_name = 'bandcamp.com', cookie_file = cookies)
+
+
+if __name__ == '__main__':
+    main()
